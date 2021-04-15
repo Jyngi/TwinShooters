b589818d780e7f86588e8b67a9adff23f50986ee
TheScribeGamer <75508009+TheScribeGamer@users.noreply.github.com>
2021-04-14 18:52:34 -0700

    Bullet Code
    Bullets now fire from player 1 and 2. Firing is currently linked to the left and right mouse buttons for testing purposes.

---
+++ TwinShooters_2/Assets/Player1BulletFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1BulletFire : MonoBehaviour
+{
+  	
+  	public Transform firePoint;
+  	public GameObject P1Bullet;
+    // Update is called once per frame
+    void Update()
+    {
+    	if (Input.GetButtonDown("Fire1"))
+    	{
+    		Fire();
+    	}
+
+    }
+
+    void Fire()
+    {
+    	Instantiate(P1Bullet, firePoint.position, firePoint.rotation);
+    }
+}
