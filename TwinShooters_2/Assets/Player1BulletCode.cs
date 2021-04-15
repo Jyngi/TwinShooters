48a625467bd8fe319b429bea708265803931754f
TheScribeGamer <75508009+TheScribeGamer@users.noreply.github.com>
2021-04-15 01:25:34 -0700

    bullet death and firing
    Created bullet death code that kills bullets when they go off screen and added a fire rate variable that lets the ships auto fire bullets. The current rate i've set them to is .7 (via the inspector)

--- TwinShooters_2/Assets/Player1BulletCode.cs
+++ TwinShooters_2/Assets/Player1BulletCode.cs
@@ -6,11 +6,12 @@ public class Player1BulletCode : MonoBehaviour
 {
     public float speed = 15f;
     public Rigidbody2D rb;
+    //Public float bulletLife = 0f;
     // Start is called before the first frame update
     void Start()
     {
     	rb.velocity = transform.up * speed;
-        
+
     }
 
     void OnTriggerEnter2d (Collider2D hitInfo)
@@ -18,6 +19,29 @@ public class Player1BulletCode : MonoBehaviour
     	Debug.Log(hitInfo.name);
     	Destroy(gameObject);
     }
+    void Update(){
+
+	    if (transform.position.y > 5f)
+	    {
+	    	Destroy(gameObject);
+	    }
+	    if (transform.position.y < -6f)
+	    {
+	    	Destroy(gameObject);
+	    }
+	    if (transform.position.x < -7.0f)
+	    {
+	    	Destroy(gameObject);
+	    }
+	    if (transform.position.x > 7.0f)
+	    {
+	    	Destroy(gameObject);
+
+	    }
+	    
+
+	   
+	}
 
 
 }
